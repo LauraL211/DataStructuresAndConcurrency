ae0da88381db2cd852797f723535148f581bd683
LauraL211 <laura.looney028@gmail.com>
2024-10-17 15:05:34 +0100

    Started the questions in Lab2 Excercises. Played around with the ArrayListDriver and updated my read me file

--- MTUSep24DSC/02. Java Collections Framework/ArrayListDriver.java
+++ MTUSep24DSC/02. Java Collections Framework/ArrayListDriver.java
@@ -3,18 +3,41 @@ import java.util.List;
 
 public class ArrayListDriver {
     public static void main(String[] args) {
-        List<String> names = new ArrayList<String>();
-        names.add("Bill"); // adds at end
-        names.add("Jill");
-        names.add("Paul");
-        names.add("Sue"); // names now has Bill, Jill, Paul, Sue
-        names.add(1,"John"); //names now has Bill, John, Jill, Paul, Sue
-        //this requires elements after Bill to be shifted
+        List<String> names = new ArrayList<>();
+        names.add("John"); // adds at end of the array list
+        names.add("Danny");
+        names.add("Nathan");
+        names.add("Conor");
+        names.add("Lara"); // creates a list(names) with John,Danny, Nathan, Conor and Lara
 
-        //output the contents of the list
-        for (int i = 0; i < names.size(); i++){
+        //Check if the names list contains Laura
+        boolean isLauraPresent = names.contains("Laura");
+
+        for(int i = 0; i < names.size(); i++) {
             String name = names.get(i);
             System.out.println(name);
         }
+        System.out.println("Array List Size:" + names.size()); //showing how many names in the array
+        System.out.println("Names list contains Laura: " + isLauraPresent + '\n');
+
+        List<String> Alternames = new ArrayList<String>();
+        Alternames.add("JJ");
+        Alternames.add("Casey");
+        Alternames.add("CJ");
+        Alternames.add("Coach");
+        Alternames.add("RoRO");
+        Alternames.add(0,"Laura"); //adding in Laura at index 0 to see how it alters the list
+        Alternames.add(6,"Laura");
+
+        //Check if the Alternames list contains Laura
+        boolean isLauraPresent2 = Alternames.contains("Laura");
+
+        //output the contents of the Alternames list
+        for(int j = 0; j < Alternames.size(); j++){
+            String Altername = Alternames.get(j);
+            System.out.println(Altername);
+        }
+        System.out.println("Array List Size:" + names.size()); //showing how many names in the array
+        System.out.println("Alternames list contains Laura: " + isLauraPresent2 + '\n');
     }
 }
--- MTUSep24DSC/README.md
+++ MTUSep24DSC/README.md
@@ -1,3 +1,6 @@
 # MTUSep24DSC
 # Making an attempt with Lab 2 - Java Collections framework
 #   Got the absolute path for the hamlet.txt file and updated it in file handling.
+
+# ArrayListDriver - I played around with the arraylist using different names. 
+# I tried counting how many names were added into the list as well as finding if my name was present
