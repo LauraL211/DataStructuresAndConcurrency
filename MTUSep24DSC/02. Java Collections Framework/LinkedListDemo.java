627bd6a3b6508c896af567e6886cac129097f538
LauraL211 <laura.looney028@gmail.com>
2024-10-17 15:47:10 +0100

    Played around with the LinkedListDemo and updated my read me file

--- MTUSep24DSC/02. Java Collections Framework/LinkedListDemo.java
+++ MTUSep24DSC/02. Java Collections Framework/LinkedListDemo.java
@@ -9,7 +9,49 @@ import java.util.ListIterator;
 public class LinkedListDemo
 {  
    public static void main(String[] args)
-   {  
+   {
+      LinkedList<String> internships = new LinkedList<>();
+      internships.add("JRI"); // JRI only in list
+      internships.add("Fexco"); // JRI, Fexco
+      internships.add("Xintec"); // JRI, Fexco, Xintec
+      internships.addLast("Continuum"); // JRI, Fexco, Xintec, Continuum(added last)
+      internships.addFirst("Aspen Grove"); // Aspen Grove(added first) JRI, Fexco, Xintec, Continuum
+      internships.add("Dell"); //  Aspen Grove, JRI, Fexco, Xintec, Continuum(added last), Dell(added after Continuum)
+      System.out.println("Current internships available:" + internships);
+
+      String internship1 = internships.getFirst();
+      System.out.println("First in the list:" + internship1); //showing what is first in the list
+
+      String internship2 = internships.getLast();
+      System.out.println("Last in the list:" + internship2); //showing what is last in the list
+
+      //Turns out Fexco already filled their placements, so it must be removed from the list
+      ListIterator<String> iterator = internships.listIterator();
+      iterator.next();
+      iterator.next();
+      iterator.next();
+      iterator.remove();
+      System.out.println("Current internships available:" + internships + '\n');
+
+      //trying to remove an item in the for loop causes errors
+      for (String s : internships){
+         System.out.println(s+"...");
+         //internships.remove(s);
+      }
+
+      //using iterator to process the contents of a linked list
+      //trying to remove items from the array works easier
+      System.out.println("Internships available before iterator:" + internships + '\n');
+      iterator = internships.listIterator();
+      while (iterator.hasNext()){
+         String name = iterator.next();
+         System.out.println(name+"...");
+         iterator.remove();
+      }
+      System.out.println("Internships available after iterator: " + internships + '\n');
+
+      System.out.println('\n');
+
       LinkedList<String> staff = new LinkedList<String>();
       staff.addLast("Diana");
       staff.addLast("Harry");
@@ -24,20 +66,20 @@ public class LinkedListDemo
       
       // | in the comments indicates the iterator position
 
-      ListIterator<String> iterator = staff.listIterator(); // |DHRT
-      iterator.next(); // D|HRT
-      iterator.next(); // DH|RT
+      ListIterator<String> iterator1 = staff.listIterator(); // |DHRT
+      iterator1.next(); // D|HRT
+      iterator1.next(); // DH|RT
 
       // Add more elements after second element
       
-      iterator.add("Juliet"); // DHJ|RT
-      iterator.add("Nina"); // DHJN|RT
+      iterator1.add("Juliet"); // DHJ|RT
+      iterator1.add("Nina"); // DHJN|RT
 
-      iterator.next(); // DHJNR|T
+      iterator1.next(); // DHJNR|T
 
       // Remove last traversed element 
 
-      iterator.remove(); // DHJN|T
+      iterator1.remove(); // DHJN|T
      
       // Print all elements
 
@@ -48,12 +90,13 @@ public class LinkedListDemo
       // Process all the elements
 
       //using iterator to process the contents of a linked list
-      iterator = staff.listIterator();
-      while (iterator.hasNext()){
-         String name = iterator.next();
+      iterator1 = staff.listIterator();
+      while (iterator1.hasNext()){
+         String name = iterator1.next();
          System.out.println(name+"...");
       }
 
+      System.out.println("\n");
       //the enhanced for loop uses an iterator behind the scenes
       for (String s : staff){
          System.out.println(s+"---");
--- MTUSep24DSC/README.md
+++ MTUSep24DSC/README.md
@@ -4,3 +4,9 @@
 
 # ArrayListDriver - I played around with the arraylist using different names. 
 # I tried counting how many names were added into the list as well as finding if my name was present
+
+# LinkedListDemo - I played around with creating my own linked list based off companies offering internships and was able to remove them as if they became unavailable.
+# Iterator vs Enhanced For Loop
+# "For-each is syntactic sugar for using iterators" - Stack Over Flow
+# Syntactic sugar supposedly is easier to read and write thus making your code that much "Sweeter"
+# Iterators are a good choice if you wish to modify what is currently in the List as trying to do so within a for loop would cause an error(attempted with my code)
